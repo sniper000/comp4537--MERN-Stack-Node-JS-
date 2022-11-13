a05d038d389ad28585092b2634f35744f399d341
Benjamin Lui <benjaminlui00@gmail.com>
2022-11-12 22:38:46 -0800

    deleted header.js

--- lab10/client/src/index.js
+++ lab10/client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Header';
 import PokeList from './PokeList';
 import 'bootstrap/dist/css/bootstrap.css';
 
